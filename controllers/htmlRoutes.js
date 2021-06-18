2dd5bc2f6fb49bc309d83f97cfcb434f0fe846f7
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-06-18 12:53:22 -0400

    ejs changes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -3,45 +3,38 @@ const path = require('path');
 const withAuth = require('../utils/auth');
 
 router.get('/', async(req, res) => {
-    let pageTitle = 'Home';
+    let pageTitle = 'home';
     res.render('index', { pageTitle });
-    // res.sendFile(path.join(__dirname, '../public/home.html'));
 });
 
 router.get('/search/:search', async(req, res) => {
-    let pageTitle = 'Home';
+    let pageTitle = 'home';
     res.render('index', { pageTitle });
-    // res.sendFile(path.join(__dirname, '../public/home.html'));
 });
 
 router.get('/item/:id', async(req, res) => {
-    let pageTitle = 'Item';
+    let pageTitle = 'item';
     res.render('item', { pageTitle });
-    // res.sendFile(path.join(__dirname, '../public/item.html'));
 });
 
 router.get('/cart', withAuth, async(req, res) => {
-    let pageTitle = 'Cart';
+    let pageTitle = 'cart';
     res.render('cart', { pageTitle });
-    // res.sendFile(path.join(__dirname, '../public/cart.html'));
 });
 
 router.get('/login', async(req, res) => {
-    let pageTitle = 'Login';
+    let pageTitle = 'login';
     res.render('login', { pageTitle });
-    // res.sendFile(path.join(__dirname, '../public/login.html'));
 });
 
 router.get('/signup', async(req, res) => {
-    let pageTitle = 'Sign-Up';
+    let pageTitle = 'signup';
     res.render('signup', { pageTitle });
-    // res.sendFile(path.join(__dirname, '../public/signup.html'));
 });
 
 router.get('/contact', async(req, res) => {
-    let pageTitle = 'Contact';
+    let pageTitle = 'contact';
     res.render('contact', { pageTitle });
-    // res.sendFile(path.join(__dirname, '../public/contact.html'));
 });
 
 module.exports = router;
